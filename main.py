9c09a83a2e7fb02ea56d1f4e79d340d5c1de02f1
slav0ntech <masqueradeit@gmail.com>
2023-08-04 12:08:06 +0300

    delete global dates env

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 from datetime import datetime, timedelta
 
 
-current_full = datetime.now()
-current_year = datetime.now().year
-current_month = datetime.now().month
-current_weekday = datetime.now().weekday()
-current_day = datetime.now().day
-
 dict_result = {
     'Monday': [],
     'Tuesdayy': [],
@@ -29,12 +23,15 @@ dict_table_weekday = {
 
 
 def get_start_point() -> datetime:
-    current_date_zero_time = current_full.replace(
-        hour=00, minute=00, second=00)  # set time 00-00-00
+
+    current_full = datetime.now()
+    current_weekday = datetime.now().weekday()
+    current_date = datetime.now().date()
+
     if current_weekday != 0:
-        start_point = current_date_zero_time - timedelta(days=current_weekday)
-        return start_point
-    return current_date_zero_time
+        current_date = current_full - timedelta(days=current_weekday)
+        return current_date
+    return current_date
 
 
 def get_end_point(start_point: datetime) -> datetime:
@@ -43,6 +40,7 @@ def get_end_point(start_point: datetime) -> datetime:
 
 
 def get_users_convert_date(users_date: datetime) -> datetime:
+    current_year = datetime.now().year
     new_date = datetime.replace(users_date, year=current_year)
     return new_date
 
@@ -62,7 +60,6 @@ def get_weekday(name_user: str, year_birthday: datetime) -> dict:
 
 def convert_dict_to_str(dict_result: dict) -> str:
     # full_result = ''
-
     # for weekday, name in dict_result.items():
     # full_result = ''.join(
     #     [f"{weekday}: {', '.join(name)} for {weekday}, {name} in {dict_result}"])
